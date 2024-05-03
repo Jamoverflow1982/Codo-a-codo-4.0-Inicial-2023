2ea7097d171ecf228e1b7fce7b99fd8a45c4945b
Javy <javy-82@hotmail.com>
2024-05-02 21:27:58 -0300

    Ejercicios clase 18

---
+++ Clase18/ejercicio1.py
@@ -0,0 +1,23 @@
+"""
+Desarrollar una función que reciba la longitud de la base y la altura de un
+triángulo, y muestre en la pantalla estos dos valores junto al área de la figura.
+Tip: el área de cualquier triángulo se calcula multiplicando la longitud de la
+base por la altura, y dividiendo el resultado entre dos.
+"""
+
+def titulo():
+    print('='*50)
+    print(' AREA DEL TRIANGULO '.center(50,'='))
+    print('='*50)
+    print()
+
+def area(base, altura):
+    total=(base*altura)/2
+    print(f' El area del triangulo es: {total:.2f} '.center(50,'-'))
+    print()
+
+titulo()
+base=float(input('Ingrese el valor de la base: '))
+altura=float(input('Ingrese el valor de la altura: '))
+print()
+area(base, altura)
---
+++ Clase18/ejercico2.py
@@ -0,0 +1,34 @@
+"""
+Desarrollar una función que reciba un número entero de 0 a 10, y que muestre
+por la terminal la tabla de multiplicar por ese número.
+Algunas ideas que puedes implementar:
+●
+●
+Verificar que el valor recibido está en el rango correcto.
+Poner un título antes de comenzar a mostrar la tabla solicitada.
+"""
+
+def titulo():
+    print('='*50)
+    print(' TABLA DE MULTIPLICAR '.center(50,'='))
+    print('='*50)
+    print()
+
+def multiplicador(num):
+    print()
+    print('-'*50)
+    print(f'{num} X 0 = {num*0}\n{num} X 1 = {num*1}\n{num} X 2 = {num*2}\n{num} X 3 = {num*3}\n{num} X 4 = {num*4}\n{num} X 5 = {num*5}\n{num} X 6 = {num*6}\n{num} X 7 = {num*7}\n{num} X 8 = {num*8}\n{num} X 9 = {num*9}\n{num} X 10 = {num*10}')
+    print('-'*50)
+
+def error(num):
+    print()
+    print('*'*50)
+    print(f' EL NUMERO {num} NO ESTA ENTRE 0 Y 10 '.center(50,'*'))
+    print('*'*50)
+
+titulo()
+num=int(input('Ingrese un numero entre el 0 al 10: '))
+if num>=0 and num<=10:
+    multiplicador(num)
+else:
+    error(num)

f5af67cc82bd9e379f9c512852fd6bf443d396ce
Javy <javy-82@hotmail.com>
2024-05-23 19:10:07 -0300

    Tabla de multiplicar

--- Clase23/ejercicio2.py
+++ Clase23/ejercicio2.py
@@ -13,6 +13,18 @@ def titulo():
     print(' by J. Monzon '.rjust(50,'*'))
     print()
 
+def validar():
+    while True:
+        numero=int(input('Ingrese el numero a multiplicar (entre 0 y 10): '))
+        if numero<0 or numero>10:
+            print()
+            print(Style.BRIGHT+Back.RED+Fore.WHITE+f'EL {numero} NO ESTA ENTRE 0 Y 10'.center(50,' '))
+            print(Style.BRIGHT+Back.RED+Fore.WHITE+'INGRESE EL NUMERO NUEVAMENTE'.center(50,' '))
+            print()
+        else:
+            break
+    return numero
+
 def multiplicar(num):
     print()
     for numm in range (num+1):
@@ -22,5 +34,4 @@ def multiplicar(num):
     print()
 
 titulo()
-numero=int(input('Ingrese el numero a multiplicar: '))
-multiplicar(numero)
+multiplicar(validar())
